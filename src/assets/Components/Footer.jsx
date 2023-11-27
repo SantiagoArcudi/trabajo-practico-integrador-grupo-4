c38969d10d776e5f5f7bf6d4aebdfb20f8b0163b
Arvant <110141548+SantiagoArcudi@users.noreply.github.com>
2023-11-27 19:21:11 +0100

    Update Footer.jsx

--- src/assets/Components/Footer.jsx
+++ src/assets/Components/Footer.jsx
@@ -12,7 +12,7 @@ const Footer = () => {
             <br />
             <span><FaLinkedin className="linkedin"/><a href="https://www.linkedin.com/in/cmrfranks?lipi=urn%3Ali%3Apage%3Ad_flagship3_profile_view_base_contact_details%3Bw3HfJ4bXT42iT%2BMEf5IVQA%3D%3D" target="_blank>Constanza Rodriguez</a></span>
             <br />
-            <span><FaLinkedin className="linkedin"/><a href="https://www.linkedin.com/in/julieta-sofia-garnero-236871121/?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=ios_app" target="_blank>Sofia Garnero</a></span>
+            <span><FaLinkedin className="linkedin"/><a href="https://www.linkedin.com/in/julieta-sofia-garnero-236871121/?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=ios_app" target="_blank">Sofia Garnero</a></span>
         </footer>
     )
 }
