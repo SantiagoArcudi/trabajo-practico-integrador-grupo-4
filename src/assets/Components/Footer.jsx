e652e9ea76d7c80b5d738a3eef6233c507a6b739
Arvant <110141548+SantiagoArcudi@users.noreply.github.com>
2023-11-27 19:19:23 +0100

    Update Footer.jsx

--- src/assets/Components/Footer.jsx
+++ src/assets/Components/Footer.jsx
@@ -6,13 +6,13 @@ import { FaLinkedin } from "react-icons/fa6";
 const Footer = () => {
     return (
         <footer>
-            <span><FaLinkedin className="linkedin"/><a href="https://www.linkedin.com/in/santiago-arcudi/">Santiago Arcudi</a></span>
+            <span><FaLinkedin className="linkedin"/><a href="https://www.linkedin.com/in/santiago-arcudi/" target="_blank>Santiago Arcudi</a></span>
             <br />
-            <span><FaLinkedin className="linkedin" /><a href="https://www.linkedin.com/in/constanzarodriguez-/">Constanza A. Rodriguez</a></span>
+            <span><FaLinkedin className="linkedin" /><a href="https://www.linkedin.com/in/constanzarodriguez-/" target="_blank>Constanza A. Rodriguez</a></span>
             <br />
-            <span><FaLinkedin className="linkedin"/><a href="https://www.linkedin.com/in/cmrfranks?lipi=urn%3Ali%3Apage%3Ad_flagship3_profile_view_base_contact_details%3Bw3HfJ4bXT42iT%2BMEf5IVQA%3D%3D">Constanza Rodriguez</a></span>
+            <span><FaLinkedin className="linkedin"/><a href="https://www.linkedin.com/in/cmrfranks?lipi=urn%3Ali%3Apage%3Ad_flagship3_profile_view_base_contact_details%3Bw3HfJ4bXT42iT%2BMEf5IVQA%3D%3D" target="_blank>Constanza Rodriguez</a></span>
             <br />
-            <span><FaLinkedin className="linkedin"/><a href="https://www.linkedin.com/in/julieta-sofia-garnero-236871121/?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=ios_app">Sofia Garnero</a></span>
+            <span><FaLinkedin className="linkedin"/><a href="https://www.linkedin.com/in/julieta-sofia-garnero-236871121/?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=ios_app" target="_blank>Sofia Garnero</a></span>
         </footer>
     )
 }
